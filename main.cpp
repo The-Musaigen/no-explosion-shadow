8f2f6c22e227f3863307b3b6686e00f78721da44
The-Musaigen <fedya.rezan@gmail.com>
2022-02-05 23:47:50 +0200

    upload

---
+++ main.cpp
@@ -0,0 +1,8 @@
+﻿#include "main.h"
+
+memwrapper::scoped_copy<5> nop_create_shadows{ 0x73747A, "\x90\x90\x90\x90\x90" };
+
+BOOL WINAPI DllMain(HMODULE hModule, DWORD dwReasonForCall, LPVOID lpvReserved)
+{
+	return TRUE;
+}
